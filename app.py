c3cdedece5e02611e37585bceb9086c0bb33f034
Ayush <22je0208@iitism.ac.in>
2023-10-07 11:54:03 +0530

    n

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask , template_rendered
 
 app = Flask(__name__)
 
-@app.route("/Hello" , methods = ["POST"])
+@app.route("/data" , methods = ["POST"])
 def hello_world():
     return {'name' : 'Ayush Barman' , 'College' : 'IIT ISM Dhanbad'}
-    # template_rendered("Hello.html")
 
 if __name__ == "__main__":
     app.run(debug=True , port = 8000)
