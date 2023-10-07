2f984b45a36b903105a08ec6608f854d2d9fe9e8
Ayush <22je0208@iitism.ac.in>
2023-10-07 11:19:05 +0530

    Added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask , template_rendered
 
 app = Flask(__name__)
 
 @app.route("/Hello")
 def hello_world():
-    return {'name' : 'Ayush Barman' , 'College' : 'IIT ISM Dhanbad'}
+    # return {'name' : 'Ayush Barman' , 'College' : 'IIT ISM Dhanbad'}
+    template_rendered("Hello.html")
 
 if __name__ == "__main__":
     app.run(debug=True , port = 8000)
