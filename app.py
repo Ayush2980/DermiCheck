52ba52e99f6d14a90fed1bdbe9036629c9efc31e
Ayush <22je0208@iitism.ac.in>
2023-10-10 00:11:25 +0530

    Hello

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask , template_rendered, jsonify
+from flask import Flask, render_template, request , jsonify
+import json
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def print():
-    template_rendered("index.html")
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 @app.route("/data")
-def hello_world():
+def do():
     result =  {'name' : 'Ayush Barman' , 'College' : 'IIT ISM Dhanbad'}
+    app.logger.info("Hello")
     return jsonify({"status" : True ,  "message" : "Found", "data" : result})
 
+
 if __name__ == "__main__":
-    app.run(debug=True , host='0.0.0.0' , port = 8000)
+    app.run(debug=True)
