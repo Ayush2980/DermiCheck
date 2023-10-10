ad0b782d46e056c400eb73e4bb728cffac990ccc
Ayush <22je0208@iitism.ac.in>
2023-10-11 01:38:43 +0530

    Updated Routing

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template, request , jsonify
+import os
 import json
 import requests
 
+Upload = 'static/'
 app = Flask(__name__)
+app.config['uploadFolder'] = Upload
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/data")
-def do():
-    result =  {'name' : 'Ayush Barman' , 'College' : 'IIT ISM Dhanbad'}
-    app.logger.info("Hello")
+@app.route("/data/<string:name>" , methods=["POST"])
+def do(name):
+    f = request.files['file']
+    f.save(os.path.join(app.config['uploadFolder'], f.filename))
+    print(f.filename)
+    result =  {'name' : name  , 'College' : 'IIT ISM Dhanbad'}
     return jsonify({"status" : True ,  "message" : "Found", "data" : result})
 
+@app.route("/predict/<string:disease>" , methods=["POST"])
+def do_it(disease):
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static", "data.json")
+    data = json.load(open(json_url))
+    return jsonify({"status" : True ,  "message" : "Found", "data" : data })
 
 if __name__ == "__main__":
     app.run(debug=True)
